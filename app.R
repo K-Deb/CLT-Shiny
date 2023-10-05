0cef15d2d4038615d1c3cc11d8ae1cb35f713d47
Debayan Koley <67727912+K-Deb@users.noreply.github.com>
2023-10-05 10:15:47 +0530

    Add files via upload

---
+++ app.R
@@ -0,0 +1,275 @@
+rm(list=ls())
+library(shiny)
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Central Limit Theorem"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      #Whether to use normal QQ plot
+      checkboxInput(inputId = "QQplot",
+                    label = "Normal Q-Q plot",
+                    value = T
+                    ),
+      checkboxInput(inputId = "Converplot",
+                    label = "Convergence plot",
+                    value = T
+      ),
+      checkboxInput(inputId = "densityplot",
+                    label = "Density curve",
+                    value = T
+      ),
+      
+      #Choosing different distributions with different parameters
+      selectInput(inputId = "popDistX",
+                  label = "Distribution",
+                  list("Normal"="normal",
+                       "Binomial"="binomial",
+                       "Poisson"="poisson",
+                       "Exponential"="exponential",
+                       "Cauchy"="cauchy",
+                       "Gamma"="gamma",
+                       "Weibull"="weibull",
+                       "Beta"="beta",
+                       "Uniform"="uniform",
+                       "Geometric"="geometric",
+                       "Logistic"="logistic",
+                       "Lognormal"="lognormal",
+                       "Negative Binomial"="negative binomial",
+                       "Hypergeometric"="hypergeometric")),
+      
+      
+      
+      #Parameter inputs for distributions
+      #For normal
+      conditionalPanel(
+        condition = "input.popDistX == 'normal'",
+        numericInput("meannorm", "Mean", min=-500, max=500, value=0, step=0.01),
+        numericInput("sdnorm", "St. dev. ", min=0.01, max=50, value=1, step=0.01)
+      ),
+      #For Binomial
+      conditionalPanel(
+        condition = "input.popDistX == 'binomial'",
+        numericInput("sizebinom", "n", min=5, max=500, value=10, step=1),
+        numericInput("probbinom", "p ", min=0.01, max=1, value=0.5, step=0.01)
+      ),
+      #For Poisson
+      conditionalPanel(
+        condition = "input.popDistX == 'poisson'",
+        numericInput("lambda", "lambda", min=0.01, max=500, value=3, step=0.01)
+      ),
+      #For Exponential
+      conditionalPanel(
+        condition = "input.popDistX == 'exponential'",
+        numericInput("rate", "lambda", min=0.01, max=500, value=3, step=0.01)
+      ),
+      #For Cauchy
+      conditionalPanel(
+        condition = "input.popDistX == 'cauchy'",
+        numericInput("locationcauchy", "location", min=-500, max=500, value=0, step=0.01),
+        numericInput("scalecauchy", "scale", min=0.01, max=50, value=1, step=0.01)
+      ),
+      #For Gamma
+      conditionalPanel(
+        condition = "input.popDistX == 'gamma'",
+        numericInput("shapegamma", "shape", min=0.01, max=500, value=2, step=0.01),
+        numericInput("rategamma", "rate", min=0.01, max=500, value=2, step=0.01)
+      ),
+      #For Weibull
+      conditionalPanel(
+        condition = "input.popDistX == 'weibull'",
+        numericInput("shapeweibull", "shape", min=0.01, max=500, value=2, step=0.01),
+        numericInput("scaleweibull", "scale", min=0.01, max=500, value=2, step=0.01)
+      ),
+      #For Beta
+      conditionalPanel(
+        condition = "input.popDistX == 'beta'",
+        numericInput("shape1", "shape1", min=0.01, max=500, value=2, step=0.01),
+        numericInput("shape2", "shape2", min=0.01, max=500, value=2, step=0.01)
+      ),
+      #For Uniform
+      conditionalPanel(
+        condition = "input.popDistX == 'uniform'",
+        numericInput("min", "min", min=-500, max=500, value=0, step=0.01),
+        numericInput("max", "max", min=-400, max=600, value=1, step=0.01)
+      ),
+      #For Geometric
+      conditionalPanel(
+        condition = "input.popDistX == 'geometric'",
+        numericInput("probgeom", "prob", min=0.01, max=1, value=0.5, step=0.01)
+      ),
+      #For Logistic
+      conditionalPanel(
+        condition = "input.popDistX == 'logistic'",
+        numericInput("locationlogistic", "location", min=-500, max=500, value=0, step=0.01),
+        numericInput("scalelogistic", "scale", min=0.01, max=500, value=1, step=0.01)
+      ),
+      #For Lognormal
+      conditionalPanel(
+        condition = "input.popDistX == 'lognormal'",
+        numericInput("meanlog", "log(mean)", min=-500, max=500, value=0, step=0.01),
+        numericInput("sdlog", "log(st.dev.)", min=0.01, max=500, value=1, step=0.01)
+      ),
+      #For negative binomial
+      conditionalPanel(
+        condition = "input.popDistX == 'negative binomial'",
+        numericInput("sizenbinom", "n", min=5, max=500, value=10, step=1),
+        numericInput("probnbinom", "p ", min=0.01, max=1, value=0.5, step=0.01)
+      ),
+      #For Hypergeometric
+      conditionalPanel(
+        condition = "input.popDistX == 'hypergeometric'",
+        numericInput("m", "Failure in Pop.", min=0, max=500, value=10, step=1),
+        numericInput("n", "Pop. Size - Failure in Pop. ", min=10, max=1000, value=50, step=1),
+        numericInput("k", "sample size ", min=5, max=100, value=20, step=1)
+      ),
+      #Input for sample size
+      numericInput(inputId = "nX",
+                   label = "Sample size",
+                   min = 40,
+                   max = 1e10,
+                   value = 100),
+      #Input for simulated sample
+      numericInput(inputId = "sim",
+                   label = "# of simulated sample",
+                   min = 10,
+                   max = 1e10,
+                   value = 100),
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 5,
+                  max = 50,
+                  value = 30)
+      #slider for sample size
+      # sliderInput(inputId = "integer",
+      #             label = "Sample size",
+      #             min = 40,
+      #             max = 10000,
+      #             value = 100),
+      #slider for simulated sample
+      # sliderInput(inputId = "integer",
+      #             label = "# of Simulated sample",
+      #             min = 10,
+      #             max = 5000,
+      #             value = 100),
+      
+      
+    ),
+    
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      fluidRow(
+        splitLayout(
+         cellWidths = c("50%","50%"),
+        tabPanel("Plot1",
+                 # Histogram output
+                 plotOutput(outputId = "Hist"),
+                 
+                 #Convergence plot
+                 plotOutput(outputId = "Convplot"),
+                 
+        ),
+        tabPanel("plot2",
+                 #QQplot
+                 plotOutput(outputId = "QQ"),
+                 
+                 #Density plot
+                 plotOutput(outputId = "Density"))
+        
+                 
+
+      )
+      
+    )
+  )
+)
+)
+
+
+# Define server logic required to draw a histogram ----
+
+server = function(input, output){
+  #set.seed(1234)
+  
+  mysamp=NULL
+  mysamp= reactive({
+    if(input$popDistX== "normal"){
+      t(replicate(input$sim,rnorm(input$nX, input$meannorm, input$sdnorm)))
+    }else if(input$popDistX== "binomial"){
+      t(replicate(input$sim,rbinom(input$nX, input$sizebinom, input$probbinom)))
+    }else if(input$popDistX== "poisson"){
+      t(replicate(input$sim,rpois(input$nX, input$lambda)))
+    }else if(input$popDistX== "exponential"){
+      t(replicate(input$sim,rexp(input$nX, input$rate)))
+    }else if(input$popDistX== "cauchy"){
+      t(replicate(input$sim,rcauchy(input$nX, input$locationcauchy,input$scalecauchy)))
+    }else if(input$popDistX== "gamma"){
+      t(replicate(input$sim,rgamma(input$nX, input$shapegamma,input$rategamma)))
+    }else if(input$popDistX== "weibull"){
+      t(replicate(input$sim,rweibull(input$nX, input$shapeweibull,input$scaleweibull)))
+    }else if(input$popDistX== "beta"){
+      t(replicate(input$sim,rbeta(input$nX, input$shape1,input$shape2)))
+    }else if(input$popDistX== "uniform"){
+      t(replicate(input$sim,runif(input$nX, input$min,input$max)))
+     }
+    else if(input$popDistX== "geometric"){
+      t(replicate(input$sim,rgeom(input$nX, input$probgeom)))}
+    else if(input$popDistX== "logistic"){
+      t(replicate(input$sim,rlogis(input$nX, input$locationlogistic,input$scalelogistic)))
+    }else if(input$popDistX== "lognormal"){
+      t(replicate(input$sim,rlnorm(input$nX, input$meanlog,input$sdlog)))
+    }else if(input$popDistX== "negative binomial"){
+      t(replicate(input$sim,rnbinom(input$nX, input$sizenbinom,input$probnbinom)))
+    }else if(input$popDistX== "hypergeometric"){
+      t(replicate(input$sim,rhyper(input$nX, input$m,input$n,input$k)))
+    }
+  })
+  meansamp=reactive(apply(mysamp(),1,mean))
+  
+  histplot=reactive({
+    bins <- seq(min(meansamp()), max(meansamp()), length.out = input$bins + 1)
+    hist(meansamp(),col="blue",main="Histogram of sample mean",xlab="Random sample",breaks = bins)
+  })
+  qqplot=reactive({
+    input$QQplot
+    if(input$QQplot){
+      qqnorm(meansamp())
+      qqline(meansamp())
+    }else {
+      (NULL)
+    }
+  })
+  converplot=reactive({
+    input$Converplot
+    if(input$Converplot){
+      plot(1:input$sim,cumsum(meansamp())/(1:input$sim),type="l",ylab="Sample mean",xlab="Sample")
+    }else {
+      (NULL)
+    }
+  })
+  densplot=reactive({
+    input$densityplot
+    if(input$densityplot){
+      plot(density(meansamp()),main="Density curve")
+      polygon(density(meansamp()),col="red")
+    }else {
+      (NULL)
+    }
+  })
+  output$Hist=renderPlot({histplot()})
+  output$QQ= renderPlot({qqplot()})
+  output$Convplot=renderPlot({converplot()})
+  output$Density=renderPlot({densplot()})
+  
+}
+
+shinyApp(ui = ui, server = server)
